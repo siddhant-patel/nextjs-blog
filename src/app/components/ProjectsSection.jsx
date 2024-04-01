81600f3bf20a647b1d1291b7cb9d2bec2af896cd
Siddhant Patel <siddhantgpatel01@gmail.com>
2024-04-01 15:49:50 -0700

    testing

---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,122 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+
+const projectsData = [
+    {
+      id: 1,
+      title: "Project 1",
+      description: "Project 1 description",
+      image: "",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 2,
+      title: "Project 2",
+      description: "Project 2 description",
+      image: "",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 3,
+      title: "Project 3",
+      description: "Project 3 description",
+      image: "",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 4,
+      title: "Project 4",
+      description: "Project 4 description",
+      image: "",
+      tag: ["All", "Mobile"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 5,
+      title: "Project 5",
+      description: "Project 5 description",
+      image: "",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 6,
+      title: "Project 6",
+      description: "Project 6 description",
+      image: "",
+      tag: ["All", "Web"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+  ];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <>
+        <h2 className="text-center text-4xl font-bold text-white mt-4">
+            My Projects
+        </h2>
+        <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+        <div className="grid md:grid-cols-3 gap-8 md:gap-12 mt-4">
+            {projectsData.map((project) => (
+                <ProjectCard 
+                    key={project.id} 
+                    title={project.title} 
+                    description={project.description} 
+                    imgUrl={project.image} 
+                    tags={project}
+                    gitUrl={project.gitUrl}
+                    previewUrl={project.previewUrl}
+            />
+            ))}
+        </div>
+    </>
+  );
+};
+
+export default ProjectsSection;
