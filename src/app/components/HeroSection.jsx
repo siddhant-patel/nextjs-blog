3233338a0fa7641667a924f5a4d7d2d6b75bf3c1
Siddhant Patel <siddhantgpatel01@gmail.com>
2024-04-01 16:48:09 -0700

    updated escape character problem in hero section

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
                 <div className="col-span-7 place-self-center text-center sm:text-left justify-self-start">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-orange-500 to-yellow-500">
-                            Hello I'm{" "}
+                            Hello I&pos;m{" "}
                         </span>
                         <br></br>
                         <TypeAnimation
