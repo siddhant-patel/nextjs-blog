81c92cd6d5fbd77f1b7a177784e45b08e0b5d619
Siddhant Patel <siddhantgpatel01@gmail.com>
2024-04-01 23:12:15 -0700

    removed the resend email thing for now

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,26 +1,26 @@
-import { Resend } from 'resend';
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [""],
-      subject: 'Hello world',
-      react: (
-        <>
-            <p>Email Body</p>
-        </>
-        ),
-    });
+// export async function POST() {
+//   try {
+//     const data = await resend.emails.send({
+//       from: fromEmail,
+//       to: [""],
+//       subject: 'Hello world',
+//       react: (
+//         <>
+//             <p>Email Body</p>
+//         </>
+//         ),
+//     });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 // import { Resend } from 'resend';
 
